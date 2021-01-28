84b8af178b6a03494b617244ab61af0e00b927b9
Vatsal Pandya <67172122+porcelaincode@users.noreply.github.com>
2021-01-28 14:23:17 +0530

    Backend Commit

--- assignmentApp/main.py
+++ assignmentApp/main.py
@@ -6,144 +6,136 @@ import time
 import sys
 import threading
 
-shelfFile = shelve.open("extras/PageDim")
 
-#Opening the blank page
-page = Image.open("page_templates/white.jpg")
+def make_assignment(fileData):
 
-def convert_to_pixels(cm, multiplicative_factor):
-    return cm*multiplicative_factor
+    images_formed = []
 
-#Getting the pixel width and height of the page
-width, height = page.size
+    shelfFile = shelve.open("assignmentApp/extras/PageDim")
+    
+    #Opening the blank page
+    page = Image.open("assignmentApp/page_templates/page.jpeg")
 
-try:
-    page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space = shelfFile["pagedimensions"]
-except:
-    print("Enter The following dimensions of the page in cm")
-    print("Enter Horizontal Length of the Page")
-    page_hor = round(float(input()))
-    print("Enter Vertical Length of the Page")
-    page_ver = round(float(input()))
-    #Converting cm values to pixels
-    horizontal_factor = width/page_hor
-    vertical_factor = height/page_ver
-    page_hor = width
-    page_ver = height
-    print("Enter Left Margin Length of the Page")
-    page_lmar = float(input())
-    page_lmar = round(convert_to_pixels(page_lmar, horizontal_factor))
-    print("Enter Top Margin Length of the Page")
-    page_tmar = float(input())
-    page_tmar = round(convert_to_pixels(page_tmar, vertical_factor))
-    print("Enter Bottom Margin Length of the Page")
-    page_bmar = float(input())
-    page_bmar = round(convert_to_pixels(page_bmar,vertical_factor))
-    print("Enter Distance Between two horizontal lines on the page (rules)")
-    page_line_space = float(input())
-    page_line_space = round(convert_to_pixels(page_line_space, vertical_factor))
+    def convert_to_pixels(cm, multiplicative_factor):
+        return cm*multiplicative_factor
 
-    #Page Size is stored in the format Page Horizontal, Page Vertical, Page Left Margin, Page Top Margin, Page Bottom Margin, Page Line Space
-    shelfFile["pagedimensions"] = [page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space]
+    #Getting the pixel width and height of the page
+    width, height = page.size
 
-#Making a copy of the blank page
-copy = page.copy()
-
-#Making ImageDraw object to add text to it
-draw = ImageDraw.Draw(copy)
-
-#Making ImageFont object of users font
-myfont = ImageFont.truetype("fonts/Snake.ttf", 105)
-
-# Open custom text document
-with open("textfiles/writing_text.txt", "r") as f:
-    text = f.read()
-
-'''
-
-#calculated pixels by measuring cm value of individual items and relating it with the pixels from the size of the page.jpg
-left_margin = 440
-top_margin = 350
-line_space = 96
-
-#start_index = 0
-space = text.index(" ")
-lwidth = 0
-
-'''
-
-page_no = 1
-
-lwidth = 0
-
-# page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space
-
-page_lmar += 10
-page_line_space -= 2.5
-#page_line_space += 10
-image_list = []
-page_lmar += 2
-page_tmar -= 15
-
-twidth = page_tmar
-
-for index, letter in enumerate(text):
     try:
-        if(letter == " "):
-            nlflag = 0
-            for i in range(index+1, len(text)):
-                if(text[i] == " "):
-                    nlflag = 1
-                    break
-            if(nlflag == 1):
-                temp_width = draw.textsize(text[index:i], myfont)[0]
-                if((lwidth+temp_width) >= (page_hor-page_lmar-100)):
-                    twidth = twidth + page_line_space
-                    lwidth = 0
-                    continue
-                    '''twidth += page_line_space
-                                                                    lwidth = 0'''
+        page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space = shelfFile["pagedimensions"]
+    except:
+        print("Enter The following dimensions of the page in cm")
+        print("Enter Horizontal Length of the Page")
+        page_hor = round(float(input()))
+        print("Enter Vertical Length of the Page")
+        page_ver = round(float(input()))
+        #Converting cm values to pixels
+        horizontal_factor = width/page_hor
+        vertical_factor = height/page_ver
+        page_hor = width
+        page_ver = height
+        print("Enter Left Margin Length of the Page")
+        page_lmar = float(input())
+        page_lmar = round(convert_to_pixels(page_lmar, horizontal_factor))
+        print("Enter Top Margin Length of the Page")
+        page_tmar = float(input())
+        page_tmar = round(convert_to_pixels(page_tmar, vertical_factor))
+        print("Enter Bottom Margin Length of the Page")
+        page_bmar = float(input())
+        page_bmar = round(convert_to_pixels(page_bmar,vertical_factor))
+        print("Enter Distance Between two horizontal lines on the page (rules)")
+        page_line_space = float(input())
+        page_line_space = round(convert_to_pixels(page_line_space, vertical_factor))
 
-        if((twidth+page_line_space >= page_ver - page_bmar + 50)):
-            temp_copy = copy.convert("RGB")
-            if(page_no == 1):
-                first_image = temp_copy
+        #Page Size is stored in the format Page Horizontal, Page Vertical, Page Left Margin, Page Top Margin, Page Bottom Margin, Page Line Space
+        shelfFile["pagedimensions"] = [page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space]
+
+    #Making a copy of the blank page
+    copy = page.copy()
+
+    #Making ImageDraw object to add text to it
+    draw = ImageDraw.Draw(copy)
+
+    #Making ImageFont object of users font
+    myfont = ImageFont.truetype("assignmentApp/fonts/Snake.ttf", 105)
+
+    # Open custom text document
+    text = fileData
+
+    page_no = 1
+
+    lwidth = 0
+
+    # page_hor, page_ver, page_lmar, page_tmar, page_bmar, page_line_space
+
+    page_lmar += 10
+    page_line_space -= 2.5
+    #page_line_space += 10
+    image_list = []
+    page_lmar += 2
+    page_tmar -= 15
+
+    twidth = page_tmar
+
+    for index, letter in enumerate(text):
+        print(index, letter)
+        try:
+            if(letter == " "):
+                nlflag = 0
+                for i in range(index+1, len(text)):
+                    if(text[i] == " "):
+                        nlflag = 1
+                        break
+                if(nlflag == 1):
+                    temp_width = draw.textsize(text[index:i], myfont)[0]
+                    if((lwidth+temp_width) >= (page_hor-page_lmar-100)):
+                        twidth = twidth + page_line_space
+                        lwidth = 0
+                        continue
+
+            if((twidth+page_line_space >= page_ver - page_bmar + 50)):
+                temp_copy = copy.convert("RGB")
+                if(page_no == 1):
+                    first_image = temp_copy
+                else:
+                    image_list.append(temp_copy)
+                copy.save(f'assignmentApp/custom/text{page_no}.png')
+                file_name = f"text{page_no}.png"
+                images_formed.append(file_name)
+                print(f"Saved page {page_no}.")
+                copy = page.copy()
+                draw = ImageDraw.Draw(copy)
+                lwidth = 0
+                twidth = page_tmar
+                page_no+=1
+
+            if((letter == "\n") or (lwidth >= (page_hor-page_lmar-100))):
+                #If there is no space to write characters
+                #start_index = index
+                lwidth = 0
+                twidth += page_line_space
+            human = random.randint(0,5)
+            add_sub = random.randint(0,1)
+            if(add_sub == 0):
+                draw.text((page_lmar+lwidth,twidth+human), letter, fill = (0,15,85,255), font = myfont)
             else:
-                image_list.append(temp_copy)
-            copy.save(f'custom/text{page_no}.png')
-            print(f"Saved page {page_no}.")
-            copy = page.copy()
-            draw = ImageDraw.Draw(copy)
-            lwidth = 0
-            twidth = page_tmar
-            page_no+=1
-
-        if((letter == "\n") or (lwidth >= (page_hor-page_lmar-100))):
-            #If there is no space to write characters
-            #start_index = index
-            lwidth = 0
-            twidth += page_line_space
-        human = random.randint(0,5)
-        add_sub = random.randint(0,1)
-        if(add_sub == 0):
-            draw.text((page_lmar+lwidth,twidth+human), letter, fill = (0,15,85,255), font = myfont)
-        else:
-            draw.text((page_lmar+lwidth,twidth-human), letter, fill = (0,15,85,255), font = myfont)
-        lwidth += draw.textsize(letter, myfont)[0]
-    except Exception as e:
-        print(f"Error Occured: {e}")
+                draw.text((page_lmar+lwidth,twidth-human), letter, fill = (0,15,85,255), font = myfont)
+            lwidth += draw.textsize(letter, myfont)[0]
+        except Exception as e:
+            print(f"Error Occured: {e}")
 
 
-copy.save(f'custom/text{page_no}.png')
-copy.convert("RGB")
-image_list.append(copy)
+    copy.save(f'assignmentApp/custom/text{page_no}.png')
+    copy.convert("RGB")
+    image_list.append(copy)
 
-try:
-    os.mkdir("documents")
-except FileExistsError:
-    pass
+    try:
+        os.mkdir("assignmentApp/documents")
+    except FileExistsError:
+        pass
 
-first_image.save(r"documents\assignment.pdf", save_all=True, append_images=image_list)
+    first_image.save(r"assignmentApp\documents\assignment.pdf", save_all=True, append_images=image_list)
 
-print("DONE!", end = "\r")
-
+    print("DONE!", end = "\r")
+    return images_formed
--- assignmentApp/views.py
+++ assignmentApp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import FileForm
+from .main import make_assignment
+
+
 def home(request):
-    if request.method == "POST":
-        print(request.POST)
-    return render(request, 'assignmentApp/home.html', {})
+    context = {}
+    if request.method == 'POST':
+        fileData = request.FILES["uploaded_file"].read().decode('ascii')
+        fileData = fileData.replace("\r","")
+        context["file_content"] = fileData
+        
+        img_path_array = make_assignment(fileData)
+        print(img_path_array)
+            
+    else:
+        print("Didnt get anything...")
+    print(context)
+    return render(request, 'assignmentApp/home.html', context)
