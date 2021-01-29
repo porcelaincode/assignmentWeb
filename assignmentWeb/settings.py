1705975ec01d659a0e2d14eac3484652612ec6a1
Vatsal Pandya <67172122+porcelaincode@users.noreply.github.com>
2021-01-29 15:50:50 +0530

    Update settings.py

--- assignmentWeb/settings.py
+++ assignmentWeb/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # STATICFILES_DIRS = (
 #     os.page.join(BASE_DIR, "/static/"),
